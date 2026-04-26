24ee6fd268fd31dcfbe37d598b8b2a16a831d519
Nicolas Tinjaca <nicolas.tinjaca@uptc.edu.co>
2026-04-26 08:59:20 -0500

    (add) soil adapter and port

---
+++ field-colector/lib/domain/ports/soil_record_remote_port.dart
@@ -0,0 +1,17 @@
+import '../entities/soil_record.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+abstract class SoilRecordRemotePort {
+  Future<void> saveSoilRecord(SoilRecord item);
+  Future<void> updateSoilRecord(String id, Map<String, dynamic> data);
+  Future<void> deleteSoilRecord(String id);
+  Future<SoilRecord?> getSoilRecordById(String id);
+  Future<SoilRecordSearchResult> getSoilRecords({int limit = 20, DocumentSnapshot? lastDocument});
+}
+
+class SoilRecordSearchResult {
+  final List<SoilRecord> items;
+  final DocumentSnapshot? lastDocument;
+
+  SoilRecordSearchResult({required this.items, this.lastDocument});
+}
